900068821c4c8ac58b16dbe4465589c44ae13292
Rogério Júnior <junior.rogerio8@gmail.com>
2019-10-22 01:27:34 -0300

    Adds test 1
    Signed-off-by: Rogério Júnior <junior.rogerio8@gmail.com>

---
+++ Prova/RogerioJunior_P1/RogerioJunior_Fisico.sql
@@ -0,0 +1,85 @@
+﻿-- --------     << Prova 01 >>     ------------
+--
+--                    SCRIPT DE CRIACAO (DDL)
+--
+-- Data Criacao ...........: 21/03/2018
+-- Autor(es) ..............: Rogério S. dos Santos Júnior
+-- Banco de Dados .........: MySQL
+-- Banco de Dados(nome) ...: RogerioJunior
+--
+-- PROJETO => 07 Base de Dados
+--         => 11 Tabelas
+--
+-- -----------------------------------------------------------------
+
+CREATE DATABASE IF NOT EXISTS RogerioJunior;
+USE RogerioJunior;
+
+CREATE TABLE CARACTERISTICA (
+    idCaracteristica INT NOT NULL AUTO_INCREMENT,
+    descricao VARCHAR(100) NOT NULL,
+
+    CONSTRAINT CARACTERISTICA_PK PRIMARY KEY (idCaracteristica)
+) ENGINE = InnoDB AUTO_INCREMENT = 1;
+
+CREATE TABLE PESSOA (
+    idPessoa  INT NOT NULL AUTO_INCREMENT,
+    primeiroNome VARCHAR(30) NOT NULL,
+    ultimoNome VARCHAR(80) NOT NULL,
+    sexo ENUM('M','F') NOT NULL,
+    dtNascimento DATE NOT NULL,
+    apelido VARCHAR(20) NOT NULL,
+
+    CONSTRAINT PESSOA_PK PRIMARY KEY (idPessoa)
+) ENGINE = InnoDB AUTO_INCREMENT = 1;
+
+CREATE TABLE INTERESSE (
+    idInteresse INT NOT NULL AUTO_INCREMENT,
+    descricao VARCHAR(100) NOT NULL,
+
+    CONSTRAINT idInteresse_PK PRIMARY KEY (idInteresse)
+) ENGINE = InnoDB AUTO_INCREMENT = 1;
+
+CREATE TABLE email (
+    idPessoa INT NOT NULL NOT NULL,
+    email VARCHAR(50) NOT NULL,
+
+    CONSTRAINT email_UK UNIQUE(email)
+) ENGINE = InnoDB;
+
+CREATE TABLE relaciona (
+    idPessoa  INT  NOT NULL,
+    idAmigo  INT NOT NULL,
+
+    CONSTRAINT relaciona_UK UNIQUE (idPessoa , idAmigo ),
+
+    CONSTRAINT relaciona_PESSOA_FK FOREIGN KEY (idPessoa)
+      REFERENCES Pessoa(idPessoa),
+    CONSTRAINT relaciona_PESSOA_amigo_FK FOREIGN KEY (idAmigo)
+      REFERENCES Pessoa(idPessoa)
+) ENGINE = InnoDB;
+
+CREATE TABLE possui (
+    idCaracteristica INT NOT NULL,
+    idPessoa  INT NOT NULL,
+
+    CONSTRAINT possui_UK UNIQUE (idCaracteristica, idPessoa ),
+
+    CONSTRAINT possui_CARACTERISTICA_FK FOREIGN KEY (idCaracteristica)
+      REFERENCES CARACTERISTICA(idCaracteristica),
+    CONSTRAINT possui_PESSOA_FK FOREIGN KEY (idPessoa)
+      REFERENCES Pessoa(idPessoa)
+) ENGINE = InnoDB;
+
+CREATE TABLE tem (
+    idPessoa INT NOT NULL,
+    idInteresse INT NOT NULL,
+    PRIMARY KEY (idPessoa, idInteresse),
+
+    CONSTRAINT tem_UK UNIQUE (idPessoa, idInteresse ),
+
+    CONSTRAINT tem_INTERESSE_FK FOREIGN KEY (idInteresse)
+      REFERENCES INTERESSE(idInteresse),
+    CONSTRAINT tem_PESSOA_FK FOREIGN KEY (idPessoa)
+      REFERENCES Pessoa(idPessoa)
+) ENGINE = InnoDB;
---
+++ Prova/RogerioJunior_P1/RogerioJunior_Popula.sql
@@ -0,0 +1,50 @@
+﻿-- --------     << Prova 01 >>     ------------
+--
+--                    SCRIPT DE POPULA (DML)
+--
+-- Data Criacao ...........: 21/03/2018
+-- Autor(es) ..............: Rogério S. dos Santos Júnior
+-- Banco de Dados .........: MySQL
+-- Banco de Dados(nome) ...: RogerioJunior
+--
+-- PROJETO => 07 Base de Dados
+--         => 11 Tabelas
+--
+-- -----------------------------------------------------------------
+
+USE RogerioJunior;
+
+INSERT INTO PESSOA VALUES
+  (NULL, 'Rogerio', 'Junior', 'M', '199-09-21', 'Roger'),
+  (NULL, 'Fabiana', 'Ribas', 'F', '1997-08-23', 'Fabi'),
+  (NULL, 'Youssef', 'Muhamad', 'M', '1999-11-20', 'You');
+
+INSERT INTO INTERESSE VALUES
+  (NULL, 'Estudar'),
+  (NULL, 'Comer'),
+  (NULL, 'Ver Netflix');
+
+INSERT INTO CARACTERISTICA VALUES
+  (NULL, 'Falador'),
+  (NULL, 'Carinhosa'),
+  (NULL, 'Simpatico');
+
+INSERT INTO email VALUES
+  (1, 'roger@email.com'),
+  (2, 'fabi@email.com'),
+  (3, 'you@email.com');
+
+INSERT INTO relaciona VALUES
+  (1,2),
+  (1,3),
+  (2,3);
+
+INSERT INTO tem VALUES
+  (1,1),
+  (2,2),
+  (3,3);
+
+INSERT INTO possui VALUES
+  (1,1),
+  (2,2),
+  (3,3);
