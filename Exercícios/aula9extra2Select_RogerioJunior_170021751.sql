bc8450831bef2917957d1f18d5c936126397521b
Rogério Júnior <junior.rogerio8@gmail.com>
2019-11-06 23:11:45 -0200

    Add class correction

--- Exercícios/aula9extra2Select_RogerioJunior_170021751.sql
+++ Exercícios/aula9extra2Select_RogerioJunior_170021751.sql
@@ -18,7 +18,8 @@ SELECT C.idReceita, M.idMedicamento, M.principioAtivo
   FROM contem as C 
   INNER JOIN MEDICAMENTO as M 
     on C.idMedicamento = M.idMedicamento 
-    WHERE C.idReceita = 2;
+    WHERE C.idReceita = 2
+ORDER BY M.principioAtivo;
 
 -- 02) Listar todas as especialidades e indicar quais médicos estão em cada uma, além das especialidades que estão sem médico
 SELECT E.nome, M.nome, M.crm, M.estado 
@@ -26,7 +27,8 @@ SELECT E.nome, M.nome, M.crm, M.estado
   LEFT JOIN possui AS P 
     INNER JOIN MEDICO AS M 
       ON P.crm = M.crm AND P.estado = M.estado 
-    ON E.idEspecialidade = P.idEspecialidade;
+    ON E.idEspecialidade = P.idEspecialidade
+ORDER BY M.nome;
 
 -- 03) Mostrar os médicos e os pacientes que foram atendidos em uma data específica
 SELECT M.nome, M.crm, M.estado, P.nome, P.cpf, C.dtHora FROM MEDICO AS M 
@@ -34,7 +36,8 @@ SELECT M.nome, M.crm, M.estado, P.nome, P.cpf, C.dtHora FROM MEDICO AS M
     ON M.crm = C.crm AND M.estado = C.estado
   INNER JOIN PACIENTE AS P 
     ON C.cpf = P.cpf
-      WHERE C.dtHora = '2009-11-28 09:30:00';
+      WHERE C.dtHora LIKE '2009-11-28%'
+ORDER BY C.dtHora;
 
 -- 04) Apresentar quantos pacientes cada médico atendeu (sem identificar os pacientes - mostrar somente a quantidade numérica)
 SELECT M.nome, M.crm, M.estado, 
@@ -42,9 +45,11 @@ SELECT M.nome, M.crm, M.estado,
     FROM MEDICO AS M 
     LEFT JOIN CONSULTA_atende AS C 
       ON M.crm = C.crm AND M.estado = C.estado
-  GROUP BY M.nome, M.crm, M.estado;
+  GROUP BY M.nome, M.crm, M.estado
+ORDER BY M.nome;
 
 -- 05) Mostrar todos os dados de pacientes consultado por parte do nome fornecido na pesquisa
 SELECT * 
   FROM PACIENTE AS P 
-    WHERE P.nome = 'Ana Claudia';
+    WHERE P.nome LIKE '%Ana%'
+ORDER BY P.nome;
